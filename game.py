95a4843a2a9daa333b63539f7c9e7f1750c7fb67
bitlii <chenjcaki@gmail.com>
2018-09-11 21:09:22 +1200

    2.0
    - Enemy with random spawn, if it hits the bottom, player loses a life. When all enemies in the game are gone, more spawn.
    - Lifepoints and score.

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 import arcade, random
 
 # TODO: PLACEHOLDERS(BG, PLAYER SPRITE, ENEMY SPRITE)
+# TODO: ENEMIES(MOVEMENT, LIFE, ATTACKING, ENEMY PATTERNS)
+# TODO: PLAYER(ABILITIES)
+# TODO: GAME("LEVELS", WIN/LOSE CONDITIONS)
+
 
 # CONSTANTS
 # SYSTEM
@@ -17,6 +21,25 @@ SPRITE_SCALING_COIN = 0.5
 COIN_COUNT = 50
 BULLET_SPEED = 15
 MOVEMENT_SPEED = 10
+
+
+class Enemy(arcade.Sprite):
+    def __init__(self, filename, sprite_scaling):
+        super().__init__(filename, sprite_scaling)
+
+        self.change_x = 0
+        self.change_y = 0
+
+    def update(self):
+        # Move the coin
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # If we are out-of-bounds, then 'bounce'
+        if self.left < 0:
+            self.change_x *= -1
+        if self.right > SCREEN_WIDTH:
+            self.change_x *= -1
 
 
 class Bullet(arcade.Sprite):
@@ -36,6 +59,7 @@ class Game(arcade.Window):
         self.bullet_sprite = None
         self.enemy_sprite = None
 
+        self.lifepoints = 5
         self.score = 0
         self.player_dx = 0
 
@@ -46,8 +70,9 @@ class Game(arcade.Window):
     def setup(self):
         self.player_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
-
         self.enemy_list = arcade.SpriteList()
+
+        self.lifepoints = 5
 
         # Player Sprite - PLACEHOLDER
         self.player_sprite = arcade.Sprite("images/playerShip1_blue.png", SPRITE_SCALING_PLAYER)
@@ -61,6 +86,12 @@ class Game(arcade.Window):
         self.player_list.draw()
         self.bullet_list.draw()
         self.enemy_list.draw()
+        # Score
+        score_text = f"Score {self.score}"
+        arcade.draw_text(score_text, 10, 20, arcade.color.WHITE, 12)
+        # Lifepoints
+        lifepoints_text = f"Life {self.lifepoints}"
+        arcade.draw_text(lifepoints_text, 10, 40, arcade.color.WHITE, 12)
 
     def on_key_press(self, key, mod):
         # Movement
@@ -90,16 +121,42 @@ class Game(arcade.Window):
                 self.player_dx = 0
 
     def update(self, delta_time):
+        if len(self.enemy_list) == 0:
+            # Enemy Spawning
+            enemy_count = random.randrange(3, 12)
+            for i in range(enemy_count):
+                enemy = Enemy("images/spider.png", SPRITE_SCALING_PLAYER)
+                enemy.center_x = random.randrange(SCREEN_WIDTH)
+                enemy.center_y = SCREEN_HEIGHT + enemy.height
+                enemy.change_x = random.randrange(-3, 4)
+                enemy.change_y = random.randrange(-4, -1)
+                self.enemy_list.append(enemy)
+
+        self.enemy_list.update()
         self.bullet_list.update()
         # Update Player Position
         self.player_sprite.center_x += self.player_dx
         # If player hits the edge of the screen
-        if self.player_sprite.center_x > SCREEN_WIDTH or self.player_sprite.center_x < 0:
+        if self.player_sprite.center_x == SCREEN_WIDTH or self.player_sprite.center_x == 0:
             self.player_dx = 0
 
         for b in self.bullet_list:
+            hit_list = arcade.check_for_collision_with_list(b, self.enemy_list)
+
             if b.bottom > SCREEN_HEIGHT:
                 b.kill()
+
+            if len(hit_list) > 0:
+                b.kill()
+
+            for e in hit_list:
+                e.kill()
+                self.score += 1
+
+        for e in self.enemy_list:
+            if e.top < 0:
+                self.lifepoints -= 1
+                e.kill()
 
 
 def main():
