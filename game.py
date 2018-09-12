9457db1c6fbd54b9d3a4a7fb304f3e5191c13a79
bitlii <chenjcaki@gmail.com>
2018-09-12 15:56:47 +1200

    3.0
    - start screen
    - lose screen
    - you can play again (cant quit though)

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import arcade, random
 
 # TODO: PLACEHOLDERS(BG, PLAYER SPRITE, ENEMY SPRITE)
-# TODO: ENEMIES(MOVEMENT, LIFE, ATTACKING, ENEMY PATTERNS)
+# TODO: ENEMIES(MOVEMENT, LIFE, ATTACKING, ENEMY PATTERNS, SFX)
 # TODO: PLAYER(ABILITIES)
-# TODO: GAME("LEVELS", WIN/LOSE CONDITIONS)
+# TODO: GAME("LEVELS", WIN CONDITIONS, MAIN MENU)
 
 
 # CONSTANTS
@@ -21,6 +21,9 @@ SPRITE_SCALING_COIN = 0.5
 COIN_COUNT = 50
 BULLET_SPEED = 15
 MOVEMENT_SPEED = 10
+
+# SOUND EFFECTS
+PLAYER_GUN_SOUND = arcade.load_sound("sfx/sfx_laser.ogg")
 
 
 class Enemy(arcade.Sprite):
@@ -63,6 +66,8 @@ class Game(arcade.Window):
         self.score = 0
         self.player_dx = 0
 
+        self.screen_view = 1
+
         self.set_mouse_visible(False)
         # Background - PLACEHOLDER
         arcade.set_background_color(arcade.color.AMAZON)
@@ -83,17 +88,27 @@ class Game(arcade.Window):
     def on_draw(self):
         arcade.start_render()
 
-        self.player_list.draw()
-        self.bullet_list.draw()
-        self.enemy_list.draw()
-        # Score
-        score_text = f"Score {self.score}"
-        arcade.draw_text(score_text, 10, 20, arcade.color.WHITE, 12)
-        # Lifepoints
-        lifepoints_text = f"Life {self.lifepoints}"
-        arcade.draw_text(lifepoints_text, 10, 40, arcade.color.WHITE, 12)
+        # Start screen
+        if self.screen_view == 1:
+            self.start_screen()
+        # Lose screen
+        elif self.screen_view == 2:
+            self.lose_screen()
+        # Game screen
+        elif self.screen_view == 0:
+            self.player_list.draw()
+            self.bullet_list.draw()
+            self.enemy_list.draw()
+
+            # Score
+            score_text = f"Score {self.score}"
+            arcade.draw_text(score_text, 10, 20, arcade.color.WHITE, 12)
+            # Lifepoints
+            lifepoints_text = f"Life {self.lifepoints}"
+            arcade.draw_text(lifepoints_text, 10, 40, arcade.color.WHITE, 12)
 
     def on_key_press(self, key, mod):
+        # GAME SCREEN
         # Movement
         if key == arcade.key.LEFT:
             self.player_dx = -MOVEMENT_SPEED
@@ -101,12 +116,23 @@ class Game(arcade.Window):
             self.player_dx = MOVEMENT_SPEED
 
         # Shoot Bullets
-        if key == arcade.key.SPACE:
+        if key == arcade.key.SPACE and self.screen_view == 0:
             bullet = Bullet("images/laserBlue01.png", SPRITE_SCALING_BULLET)
             bullet.center_x = self.player_sprite.center_x
             bullet.bottom = self.player_sprite.top
 
             self.bullet_list.append(bullet)
+            arcade.play_sound(PLAYER_GUN_SOUND)
+
+        # START SCREEN
+        if key == arcade.key.SPACE and self.screen_view == 1:
+            # Game Screen view
+            self.screen_view = 0
+
+        # LOSE SCREEN
+        if key == arcade.key.SPACE and self.screen_view == 2:
+            self.setup()
+            self.screen_view = 1
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT:
@@ -121,44 +147,69 @@ class Game(arcade.Window):
                 self.player_dx = 0
 
     def update(self, delta_time):
-        if len(self.enemy_list) == 0:
-            # Enemy Spawning
-            enemy_count = random.randrange(3, 12)
-            for i in range(enemy_count):
-                enemy = Enemy("images/spider.png", SPRITE_SCALING_PLAYER)
-                enemy.center_x = random.randrange(SCREEN_WIDTH)
-                enemy.center_y = SCREEN_HEIGHT + enemy.height
-                enemy.change_x = random.randrange(-3, 4)
-                enemy.change_y = random.randrange(-4, -1)
-                self.enemy_list.append(enemy)
+        if self.lifepoints > 0 and self.screen_view == 0:
+            if len(self.enemy_list) == 0:
+                # Enemy Spawning
+                enemy_count = random.randrange(3, 12)
+                for i in range(enemy_count):
+                    enemy = Enemy("images/spider.png", SPRITE_SCALING_PLAYER)
+                    enemy.center_x = random.randrange(SCREEN_WIDTH)
+                    enemy.center_y = SCREEN_HEIGHT + enemy.height
+                    enemy.change_x = random.randrange(-3, 4)
+                    enemy.change_y = random.randrange(-4, -1)
+                    self.enemy_list.append(enemy)
 
-        self.enemy_list.update()
-        self.bullet_list.update()
-        # Update Player Position
-        self.player_sprite.center_x += self.player_dx
-        # If player hits the edge of the screen
-        if self.player_sprite.center_x == SCREEN_WIDTH or self.player_sprite.center_x == 0:
-            self.player_dx = 0
+            self.enemy_list.update()
+            self.bullet_list.update()
+            # Update Player Position
+            self.player_sprite.center_x += self.player_dx
+            # If player hits the edge of the screen
+            if self.player_sprite.center_x == SCREEN_WIDTH or self.player_sprite.center_x == 0:
+                self.player_dx = 0
 
-        for b in self.bullet_list:
-            hit_list = arcade.check_for_collision_with_list(b, self.enemy_list)
+            for b in self.bullet_list:
+                hit_list = arcade.check_for_collision_with_list(b, self.enemy_list)
 
-            if b.bottom > SCREEN_HEIGHT:
-                b.kill()
+                if b.bottom > SCREEN_HEIGHT:
+                    b.kill()
 
-            if len(hit_list) > 0:
-                b.kill()
+                if len(hit_list) > 0:
+                    b.kill()
 
-            for e in hit_list:
-                e.kill()
-                self.score += 1
+                for e in hit_list:
+                    e.kill()
+                    self.score += 1
 
-        for e in self.enemy_list:
-            if e.top < 0:
-                self.lifepoints -= 1
-                e.kill()
+            for e in self.enemy_list:
+                if e.top < 0:
+                    self.lifepoints -= 1
+                    e.kill()
+        elif self.screen_view == 1:
+            pass
+        else:
+            self.screen_view = 2
+
+    # SCREENS
+    # Start Screen
+    def start_screen(self):
+        # Base Rectangle
+        arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 600, 300, arcade.color.WHITE)
+        # "text", start_x, start_y, colour, size, alignment, x anchor point, y anchor point
+        arcade.draw_text(("SPACE TO START"), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, arcade.color.AIR_FORCE_BLUE, 50, align="center", anchor_x="center", anchor_y="center")
+
+    # Lose Screen
+    def lose_screen(self):
+        # Main text
+        arcade.draw_text(("YOU LOSE"), SCREEN_WIDTH / 2, 415, arcade.color.BLACK, 77, align="center", anchor_x="center", anchor_y="center")
+        # Play Again Button
+        arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, 310, 400, 100, arcade.color.GREEN)
+        arcade.draw_text(("Space to Play Again"), SCREEN_WIDTH / 2, 310, arcade.color.WHITE, 25, align="center", anchor_x="center", anchor_y="center")
+        # Quit Button
+        arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, 205, 400, 100, arcade.color.RED)
+        arcade.draw_text(("Escape to Quit"), SCREEN_WIDTH / 2, 205, arcade.color.WHITE, 25, align="center", anchor_x="center", anchor_y="center")
 
 
+# Main function to start the game window
 def main():
     window = Game()
     window.setup()
